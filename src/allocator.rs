8078d6b3970ea1efd07f8f41725513cefa0f0969
Moonix1 <bzsolczay@gmail.com>
2024-11-13 23:01:57 +1100

    Added alloc!

--- src/allocator.rs
+++ src/allocator.rs
@@ -1,15 +1,31 @@
-// WIP
-
 use core::{alloc::{
 	GlobalAlloc, Layout,
-}, sync::atomic::AtomicPtr};
+}, sync::atomic::{AtomicPtr, Ordering}};
 
-use uefi::mem::memory_map::MemoryMapOwned;
+use boot::MemoryType;
+use log::info;
+use uefi::{mem::memory_map::{MemoryMap, MemoryMapOwned}, prelude::*, println};
 
+#[repr(C, packed)]
 struct FreeSegment {
 	size: usize,
-	offset: usize,
 	next_segment: *mut FreeSegment,
+}
+
+impl FreeSegment {
+	fn get_start(&self) -> *mut u8 {
+		unsafe { (self as *const FreeSegment).add(1) as *mut u8 }
+	}
+
+	fn get_end(&self) -> *mut u8 {
+		unsafe { self.get_start().add(self.size) }
+	}
+}
+
+#[repr(C, packed)]
+struct UsedSegment {
+	size: usize,
+	padding: [u8 ; 8],
 }
 
 pub struct Allocator {
@@ -22,13 +38,73 @@ impl Allocator {
 	}
 
 	pub fn init(&self, mmap: MemoryMapOwned) {
+		assert_eq!(core::mem::size_of::<UsedSegment>(), core::mem::size_of::<FreeSegment>());
 
+		let big_block = mmap.entries().find(|entry| {
+			entry.ty == MemoryType::LOADER_DATA
+		}).expect("Failed to find big block of RAM!");
+
+		let big_block_start = big_block.phys_start as *mut u8;
+		let big_block_size = big_block.page_count * 4096;
+
+		let segment_start = unsafe { big_block_start.add(core::mem::size_of::<FreeSegment>()) };
+
+		let segment_size = big_block_size as usize - core::mem::size_of::<FreeSegment>();
+
+		let segment = segment_start as *mut FreeSegment;
+		unsafe {
+			(*segment).size = segment_size;
+			(*segment).next_segment = core::ptr::null_mut();
+		}
+
+		self.first_free.store(segment, Ordering::Relaxed);
 	}
+}
+
+unsafe fn get_header_ptr(segment: &FreeSegment, layout: &Layout) -> Option<*mut u8> {
+	let segment_start = segment.get_start();
+	let segment_end = segment.get_end();
+
+	let mut ptr = segment_end.sub(layout.size());
+	ptr = ptr.sub((ptr as usize) % layout.align());
+	ptr = ptr.sub(core::mem::size_of::<UsedSegment>());
+	
+	if ptr < segment_start {
+		return None
+	}
+
+	Some(ptr)
 }
 
 unsafe impl GlobalAlloc for Allocator {
 	unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-		unimplemented!()
+		let mut free_block_it = self.first_free.load(Ordering::Relaxed);
+
+		while !free_block_it.is_null() {
+			let header_ptr = get_header_ptr(&*free_block_it, &layout);
+			let header_ptr = match header_ptr {
+				Some(v) => v,
+				None => {
+					free_block_it = (*free_block_it).next_segment;
+					continue;
+				}
+			};
+
+			let segment_end = (*free_block_it).get_end();
+
+			let new_size = header_ptr.offset_from((*free_block_it).get_start());
+			let new_size = new_size.try_into().expect("expected valid usize for new fragment size");
+			(*free_block_it).size = new_size;
+
+			let header_ptr = header_ptr as *mut UsedSegment;
+			(*header_ptr).size = segment_end.offset_from(header_ptr.add(1) as *const u8)
+				.try_into()
+				.expect("expected segment end offset to be castable to usize");
+
+			return header_ptr.add(1) as *mut u8;
+		}
+
+		panic!("Failed to allocate!");
 	}
 
 	unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 #![no_main]
 #![no_std]
 
-use core::{mem, panic::PanicInfo};
+use core::{mem, num::NonZero, panic::PanicInfo, ptr::NonNull};
 
 extern crate uefi;
 extern crate alloc;
 
 mod allocator;
 
-use alloc::vec::Vec;
+use alloc::vec::*;
+use alloc::vec;
 use boot::MemoryType;
 use uefi::{mem::memory_map::MemoryMap, prelude::*};
 use log::*;
@@ -16,26 +17,54 @@ use log::*;
 #[global_allocator]
 static ALLOC: allocator::Allocator = allocator::Allocator::new();
 
+fn setup_stack(stack_size: usize) -> (Result<NonNull<u8>, Status>, NonNull<u8>) {
+	let aligned_size = (stack_size + 4095) & !4095;
+	let stack_ptr =
+		boot::allocate_pages(
+			boot::AllocateType::AnyPages,
+			MemoryType::LOADER_DATA,
+			aligned_size / 4096
+		).map_err(|e| e.status());
+
+	let stack_end = unsafe { stack_ptr.unwrap().add(aligned_size) };
+
+	(Ok(stack_ptr.unwrap()), stack_end)
+}
+
 #[entry]
 fn main() -> Status {
     uefi::helpers::init().unwrap();
+
+	// Allocate 1MB of stack space
+	let (stack_ptr, stack_end) = setup_stack(1 * 1024 * 1024);
+	
 	let mmap = boot::memory_map(MemoryType::LOADER_DATA).unwrap();
 	let mut total_len = 0;
 	for i in 0..mmap.len() {
 		let desc = &mmap[i];
 		let len = desc.page_count * 4096;
 		total_len += len;
-		info!("size {}, len: {}, addr: {}", size_of_val(desc), len, desc.phys_start);
+		info!("size {}, len: {}, addr: {:#X}", size_of_val(desc), len, desc.phys_start);
 	}
 
 	info!("{}MB", total_len / 1024 / 1024);
+	info!("stack start: {:#X}", stack_ptr.unwrap().addr());
+	info!("stack end: {:#X}", stack_end.addr());
 	
 	ALLOC.init(mmap);
 
+	let mut v = vec![
+		123453,
+		69696,
+		23453,
+		245436455
+	];
+
+	info!("{:?}", v);
 
     trace!("Hello world!");
-    boot::stall(10_000_000);
-    Status::SUCCESS
+    
+	loop {}
 }
 
 #[panic_handler]
